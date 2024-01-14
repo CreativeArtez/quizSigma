e93cfe441b21377b4757edc6d6b395bc584126f8
Art <125709882+CreativeArtez@users.noreply.github.com>
2024-01-14 13:28:43 +0700

    Add files via upload

---
+++ quiz2.js
@@ -0,0 +1,173 @@
+
+let questions = [
+     {
+     numb: 1,
+     question: "3x + 5 = 20 berapa x nya",
+     answer: "5",
+     options: [
+       "1",
+       "2",
+       "5",
+       "6"
+     ]
+   },
+     {
+     numb: 2,
+     question: "4 - 20 x 3 + 5 =",
+     answer: "-51",
+     options: [
+       "-51",
+       "71",
+       "51",
+       "-71"
+     ]
+   },
+     {
+     numb: 3,
+     question: "apa yang kau lakukan saat materi ujian matematika tidak diajarkan oleh guru mu",
+     answer: "tunggu guru keluar lalu kerjasama/nyontek",
+     options: [
+       "mengerjakan sebisa mungkin",
+       "tunggu guru keluar lalu kerjasama/nyontek",
+       "pasrah dan berdiam diri saja",
+       "lapor ke ortu untuk meminta pertanggung jawaban"
+     ]
+   },
+     {
+     numb: 4,
+     question: "5x + 7y = 4  jika x=5 berapa y nya? ",
+     answer: "-3",
+     options: [
+       "tak terdefinisi",
+       "1",
+       "3",
+       "-3"
+     ]
+   },
+     {
+     numb: 5,
+     question: "mengapa banyak murid benci matemaika",
+     answer: "karena guru yang tak bertanggung jawab",
+     options: [
+       "karena susah dipelajari",
+       "karena guru yang tak bertanggung jawab",
+       "karena matematika gak bisa montage",
+       "karena siswa tak mampu bernalar logis"
+     ]
+   },
+  {
+    numb:6 ,
+    question: "apa fungsi dari css",
+    answer: "pengembangan terhadap design dan mengubab content di web",
+    options: [
+      "untuk membangun website yang cantik",
+      "pengembangan terhadap design dan mengubab content di web",
+      "agar website dapat berjalan sesuai standard yang dibuat",
+      "supaya website lebih aman dari serangan cyber atau hacker"
+    ]
+  },
+  {
+    numb: 7,
+    question: "apa itu framework dalam bahasa pemrogaman",
+    answer: "kerangka kerja yang dibuat untuk membangun web dengan lebih efisien",
+    options: [
+      "jenis bahasa pemrogaman yang populer untuk pemula",
+      "kerangka kerja yang dibuat untuk membangun web dengan lebih efisien",
+      "kerangka kerja untuk membangun design dengan cantik dan artistik",
+      "kerangka kerja cerdas yang bisa meng-generate kode"
+    ]
+  },
+  {
+    numb:8 ,
+    question: "apa aplikasi untuk belajar/kerja di dunia pemrogaman",
+    answer: "Visual Studio Code",
+    options: [
+      "Notepad",
+      "microsoft Excel",
+      "Visual Studio Code",
+      "Oracle code "
+    ]
+  },
+  {
+    numb: 9,
+    question: "apakah efisien ngoding di kertas?",
+    answer: "tidak, karena itu lawak",
+    options: [
+      "tentu, karna tak membutuhkan komputer",
+      "tentu, supaya siswa paham setiap detail kode",
+      "tidak, karena itu lawak",
+      "tidak, karna tak membutuhkan komputer"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Dalam layer OSI , website ada di layer mana?",
+    answer: "Application Layer",
+    options: [
+      "Physical layer",
+      "Data Link layer",
+      "Application Layer",
+      "Session layer"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Apa yang sering di kampanyekan oleh calon wakil rakyat?",
+    answer: "Janji",
+    options: [
+      "Janji",
+      "Narasi dan Gagasan dengan Data",
+      "Pidato Perusasif",
+      "Kebaikan dan Kemashalatan untuk semua"
+    ]
+  },
+  {
+    numb: 12,
+    question: "siapa yang membuat web ini",
+    answer: "Web developer",
+    options: [
+      "Mamamk gacor",
+      "R1fqi asli",
+      "Antarezz",
+      "Web developer"
+    ]
+  },
+  {
+    numb: 13,
+    question: "apakah kalian bisa membuat Web ini",
+    answer: "Pasti bisa",
+    options: [
+      "Saya tak yakin",
+      "Pasti bisa",
+      "Saya tak bisa",
+      "jangan pakai notepad"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Apa materi yang termuat Dalam Makalah Spooky action at a distace?",
+    answer: "berisi laporan penelitian mekanika kuantum",
+    options: [
+      "berisi mitos mitos di asia",
+      "berisi agenda para elit global",
+      "berisi laporan Astronomi dan objek exo planet",
+      "berisi laporan penelitian mekanika kuantum"
+    ]
+  },
+  {
+    numb: 15,
+    question: "apa singkatan dari SGIE?",
+    answer: "State Global Islamic Economy",
+    options: [
+      "Station Global in Europe",
+      "hanya cak imin yang tau",
+      "State Global Islamic Economy",
+      "Statue General Islamic Economy"
+    ]
+  },
+  
+ 
+ ];
+
+
+
